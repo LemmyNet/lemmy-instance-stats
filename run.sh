49082e3f07067c708b080aaf29682d1f0a9ac7df
Dessalines <tyhou13@gmx.com>
2022-01-31 17:44:02 -0500

    Revert "Add 20 crawling depth."
    This reverts commit 0d14431997ed33cf9f8946a30c1f73f040f07293.

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 pushd ../lemmy-stats-crawler
-cargo run -- --max-crawl-depth 20 --start-instances lemmy.ml,wolfballs.com --exclude ds9.lemmy.ml,voyager.lemmy.ml,enterprise.lemmy.ml > stats.json
+cargo run -- --start-instances lemmy.ml,wolfballs.com --exclude ds9.lemmy.ml,voyager.lemmy.ml,enterprise.lemmy.ml > stats.json
 mv stats.json ../lemmy-instance-stats
 popd
