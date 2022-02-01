a0dcf4b97f7b2d79dbc62e5728a6bb55eb566bf1
Dessalines <tyhou13@gmx.com>
2022-02-01 10:12:52 -0500

    Exclude lemmy.glasgow.social, all unmarked bot accts.

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 pushd ../lemmy-stats-crawler
-cargo run -- --start-instances lemmy.ml --exclude ds9.lemmy.ml,voyager.lemmy.ml,enterprise.lemmy.ml > stats.json
+cargo run -- --start-instances lemmy.ml --exclude lemmy.glasgow.social,ds9.lemmy.ml,voyager.lemmy.ml,enterprise.lemmy.ml > stats.json
 mv stats.json ../lemmy-instance-stats
 popd
